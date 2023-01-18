34f409cb2e75cce931bf5db5690cc0b32a4b15ff
Malvarez4204 <106292326+Malvarez4204@users.noreply.github.com>
2023-01-17 20:36:33 -0600

    Se agregan comentarios de uso de variables

--- assets/js/mani.js
+++ assets/js/mani.js
@@ -1,7 +1,22 @@
+"use strict";
+
 console.log("Hola mundo 1");
 console.log('Hola mundo 2');
 console.log("Hola mundo 3"); 
 
 var nombre = "Juan";
-var edad= 30;
+var edad = 30;
+const fechaNacimiento = "14/11/1994"; //var es una constante que no deberia cambiar 
+let apellido = "Perez";
+apellido = "Gomez"
+//para reasignar solo se pone el nombre de la variable
+
+console.log(apellido);
+console.log(edad);
+console.log(nombre)
+
+// <!-- esta etiqueta script contiene javascript ocupa existir antes de ser usado debe cargar en el nivel mas bajo posible-->
+// <!-- se recomienda camelCase para js -->
+// siempre se cierran con ; toda linea , aun que la linea brinda renglon
+// solo usar let / const para variables, var ya no se usa 
 
