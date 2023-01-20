89a0d03e138ca9f869e86eb5171974fb04e776f5
Malvarez4204 <106292326+Malvarez4204@users.noreply.github.com>
2023-01-19 20:02:44 -0600

    Mensaje alerta

--- assets/js/ejercicio.js
+++ assets/js/ejercicio.js
@@ -1,8 +1,18 @@
 console.log("Primera prueba")
 
 let carName = "Volvo";
+let driver = "Juan"
 let x = 50;
 let a = 5;
 let b = 10;
 let suma = a + b;
 console.log("La suma es ", suma);
+let concatenacion2 = driver + " " +carName;
+let concatenacion = carName + driver;
+console.log (concatenacion);
+console.log (concatenacion2);
+
+//string template en javascriopt
+console.log ("El conductor" + " " + driver + " " + "Tiene un" + " " + carName);
+
+let optimizacionConcatenacion = ´El conductor ${driver} tiene un ${carName}´';
---
+++ assets/js/operadores.js
@@ -0,0 +1,14 @@
+console.log("Esto es el archivo de operadores");
+
+let numero1 = 10;
+let numero2 = 15;
+
+console.log(`la suma: ${numero1 + numero2}`);
+console.log(`la resta: ${numero1 - numero2}`);
+console.log(`la multiplicacion: ${numero1 * numero2}`);
+console.log(`la multiplicacion: ${numero1 / numero2}`);
+
+//se utiliza $ para evitar el uso de sumas en el template +++ alt Gr + el corchete que cierra 
+//NaN (not a number )
+alert(`Se esta descargando un virus en tu equipo`);
+alert(`Al dar aceptar aceptas la descarga`);
